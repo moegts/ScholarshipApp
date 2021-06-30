f8a50f5c7775475d3d3f4ddc6efc005455623ff8
gtssss <>
2021-06-30 19:03:31 +0000

    update and add js files

---
+++ app.js
@@ -0,0 +1,16 @@
+let btnRed = document.querySelector('#btn-red');
+let btnGray = document.querySelector('#btn-gray');
+let btnBlue = document.querySelector('#btn-blue');
+let dropdown = document.querySelector('select');
+
+btnRed.addEventListener('click', () => {
+  document.body.style.background='red';
+})
+
+btnGray.addEventListener('click', () => {
+  document.body.style.background='#2c2f33';
+})
+
+btnBlue.addEventListener('click', () => {
+  document.body.style.background='blue';
+})
---
+++ askName.js
@@ -0,0 +1,21 @@
+var test = prompt("Security test: what is 2+3= ?")
+console.log(test)
+if(test == "5")
+{
+  alert("you are not a bot!")
+}
+else if (test!= "")
+{
+  alert("you are bot! I will send you to ask Google :)")
+  location.replace("https://www.Google.com")
+}
+else{
+  alert("you are bot! I will send you to ask Google :)")
+  location.replace("https://www.Google.com")
+}
+
+var name = prompt("Hello, What is your Name?")
+console.log(name)
+{
+  document.write("<h2>Welcome to This Scholarship " + name + "</h2>")
+}
