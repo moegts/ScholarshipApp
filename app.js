6d8466096f7525f71ff51f3bc85739c4e6a2c675
gtssss <>
2021-07-04 21:52:56 +0000

    auto push

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ else{
   location.replace("https://www.Google.com")
 }
 
-
+// zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzx //
 // i want to ask the user what mode you want the page to be on dark or light mode
 // for collors etc
 // ask him
@@ -38,38 +38,57 @@ else{
 // while(res == strs){
 // res= alert("good")
 // }
-
+// //-----------------------------------------------------------------//
 alert("Welcome to the Scholarship of Software Development page!")
 
 var x = prompt("what program language you like the most Python, Javascript OR C++?")
-var xx = x.toLowerCase();
+var zz = x.toLowerCase();
 
-while (xx != "python" && xx != "py" && xx != "javascript" && xx != "js" && xx != "c++")
-{xx =prompt("please type the program you want (Python or Py ,Javascript or Js ,C++)")}
+while (zz != "python" && zz != "py" && zz != "javascript" && zz != "js" && zz != "c++")
+{zz =prompt("please type the program you want (Python or Py ,Javascript or Js ,C++)")}
 
-var y = prompt("How many Copys you want of " + xx + "? ")
+var y = prompt("How many Copys you want of " + zz + "? ")
 
 while(y > 10)
 
-{y = prompt("How many " + xx + " you want? (Please put number between [1 ,10] ")}
+{y = prompt("How many " + zz + " you want? (Please put number between [1 ,10] ")}
 
 for(var i=1;i<=y ;i++ ){
 
-  if(xx =="python" ){
+  if(zz =="python" ){
  document.write("<h3>"+ i + " of Python"+ "</h3>" + "<img src='photos/py.jpg' alt='python'>")
  }
-  if(xx =="py" ){
+  if(zz =="py" ){
  document.write("<h3>"+ i + " of Python"+ "</h3>" + "<img src='photos/py.jpg' alt='python'>")
  }
-  if(xx =="javascript" ){
+  if(zz =="javascript" ){
  document.write("<h3>"+ i + " of Javascript"+ "</h3>" + "<img src='photos/js.jpg' alt='javascript'>")
  }
-  if(xx =="js" ){
+  if(zz =="js" ){
  document.write("<h3>"+ i + " of Javascript"+ "</h3>" + "<img src='photos/js.jpg' alt='javascript'>")
  }
-  if(xx =="c++" ){
+  if(zz =="c++" ){
  document.write("<h3>"+ i + " of C++"+ "</h3>" + "<img src='photos/cpp.jpg' alt='cpp'>")
  }
  
 }
 
+
+function Num2(){
+var num2= prompt("enter how much of pepsi you want?(5)");
+var zz=false;
+while(zz==false) {
+if(num2<1 ){
+zz=false;
+num2=prompt("enter how much of pepsi you want?(5)");
+}
+else if(num2>5) {
+zz=false;
+num2=prompt("Enter a number between 1-5");
+}
+else
+zz=true;
+
+}
+return num2;
+}
